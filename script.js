b43d8813efada57f649d73e35a7be1122b971af7
PrimoCrypt <yhungdew@gmail.com>
2022-06-24 12:18:29 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const num1 = parseFloat(prompt("Enter first number: "));
+const operators = prompt("Enter operators (either +,-,* or /)");
+const num2 = parseFloat(prompt("Enter second number: "));
+
+let result;
+if (operators == "+"){
+    result = num1 + num2;
+}
+else if (operators == "-") {
+    result = num1 - num2;
+}
+else if (operators == "*") {
+    result = num1 * num2;
+}
+else if (operators == "/") {
+    result = num1 / num2;
+}
+alert(result);
